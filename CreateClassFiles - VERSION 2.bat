d086f7a95c5f3e35e89555de404bc6e423b68433
Dylan <djlevy26@gmail.com>
2025-05-19 18:17:12 -0400

    fix: add all updated files

---
+++ CreateClassFiles - VERSION 2.bat
@@ -0,0 +1,4 @@
+%COMSPEC% /c "javac.exe" *.java
+
+REM After you run this file, confirm that there are now .class versions of all of your .java files
+PAUSE
---
+++ DeleteClassFiles.bat
@@ -0,0 +1,2 @@
+cmd /c "del /S *.class"
+exit
